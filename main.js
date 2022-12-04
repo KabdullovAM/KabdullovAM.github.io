94807e6dd56397a9dd72f8ed9c98869cd772ec2f
Aldiyar <ualdiyar@gmail.com>
2022-12-04 19:22:51 +0600

    1

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("Hello, World!");
+
+let h1_arr=document.getElementsByTagName("h1")
+console.log(h1_arr[0])
+
+let h=h1_arr[0]
+h.style.color="green"
+h.setAttribute("style","color: green;")
+
+let=hr=document.createElement("hr")
+console.log(hr)
+document.body.appendChild(hr)
